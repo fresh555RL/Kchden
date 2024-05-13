a3a0337f1d5ca1e73366f40aae25654704ad77ef
fresh555RL <129037326+fresh555RL@users.noreply.github.com>
2024-05-13 19:36:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+    var styleProps = $("#b1ton" ).css([ "width", "height", "color", "background-color" ]);
+    console.log(styleProps);
+    $(document).ready(function(){
+
+        $("#b1ton").click(function(){
+
+        $("#b1ton p").toggle();
+        });
+
+
+        $("#f").click(function(){
+            $("#f").before($("#s"));
+        });
+        $("#s").click(function(){
+            $("#s").after($("#b1ton"));
+        });
+        $( "#foo" ).bind( "mouseenter ", function() {
+            $( this ).toggleClass( "entered" );
+            $(this).text("Пиздец... забеременела");
+        });
+        $( "#foo" ).bind( "mouseleave", function() {
+            $( this ).toggleClass( "entered" );
+            $(this).text("Жду алименты...");
+        });
+
+        var styleProps = $("#b1ton").css("width");
+        console.log(styleProps);
+        //   $("button").click(function(){
+        //     // console.log($(this).css());
+
+
+        //   });
+    });
